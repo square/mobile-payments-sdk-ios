d056c4aa897c2f0f2c24cca4f54ee2afb4cf7165
Elton Gao <yuanfeng@squareup.com>
2025-08-15 16:46:58 -0400

    Add four langs to demo Mobile payment SDK strings are localized as well

--- Example/DonutCounter/DonutCounter/Extensions/String+Extensions.swift
+++ Example/DonutCounter/DonutCounter/Extensions/String+Extensions.swift
@@ -3,45 +3,45 @@ import Foundation
 extension String {
     
     enum Home {
-        static var appTitle: String = "Donut Counter"
-        static var notAuthorized: String = "Device not authorized. Open permissions to authorize."
-        static var permissionsButtonTitle: String = "Permissions"
-        static var settingsButtonTitle: String = "Settings"
-        static var buyDonutButtonTitle: String = "Buy for $1"
+        static var appTitle: String = NSLocalizedString("Donut Counter", comment: "Main app title")
+        static var notAuthorized: String = NSLocalizedString("Device not authorized. Open permissions to authorize.", comment: "Message shown when device is not authorized")
+        static var permissionsButtonTitle: String = NSLocalizedString("Permissions", comment: "Title for permissions button")
+        static var settingsButtonTitle: String = NSLocalizedString("Settings", comment: "Title for settings button")
+        static var buyDonutButtonTitle: String = NSLocalizedString("Buy for $1", comment: "Title for buy donut button")
         
         enum PaymentStatusAlert {
-            static var paymentCompletedTitle: String = "Payment Completed!"
-            static var paymentFailedTitle: String = "Payment Failed!"
-            static var paymentCanceledTitle: String = "Payment Canceled"
+            static var paymentCompletedTitle: String = NSLocalizedString("Payment Completed!", comment: "Title for payment completed alert")
+            static var paymentFailedTitle: String = NSLocalizedString("Payment Failed!", comment: "Title for payment failed alert")
+            static var paymentCanceledTitle: String = NSLocalizedString("Payment Canceled", comment: "Title for payment canceled alert")
         }
         
         enum MockReaderButton {
-            static var showMockReaderTitle: String = "Show Mock Reader"
-            static var hideMockReaderTitle: String = "Hide Mock Reader"
+            static var showMockReaderTitle: String = NSLocalizedString("Show Mock Reader", comment: "Title for show mock reader button")
+            static var hideMockReaderTitle: String = NSLocalizedString("Hide Mock Reader", comment: "Title for hide mock reader button")
         }
     }
     
     enum Permissions {
-        static var headerTitle: String = "Permissions"
+        static var headerTitle: String = NSLocalizedString("Permissions", comment: "Permissions screen header title")
         
         enum Bluetooth {
-            static var bluetoothPermissionTitle: String = "Bluetooth"
-            static var bluetoothPermissionDescription: String = "Square uses Bluetooth to connect and communicate with Square devices. \nYou should ask for this permission if you are using readers that connect via Bluetooth."
+            static var bluetoothPermissionTitle: String = NSLocalizedString("Bluetooth", comment: "Bluetooth permission title")
+            static var bluetoothPermissionDescription: String = NSLocalizedString("Square uses Bluetooth to connect and communicate with Square devices. \nYou should ask for this permission if you are using readers that connect via Bluetooth.", comment: "Bluetooth permission description")
         }
         
         enum Location {
-            static var locationPermissionTitle: String = "Location"
-            static var locationPermissionDescription: String = "Square uses location to know where transactions take place. This reduces risk and minimizes payment disputes."
+            static var locationPermissionTitle: String = NSLocalizedString("Location", comment: "Location permission title")
+            static var locationPermissionDescription: String = NSLocalizedString("Square uses location to know where transactions take place. This reduces risk and minimizes payment disputes.", comment: "Location permission description")
         }
         
         enum Microphone {
-            static var microphonePermissionTitle: String = "Microphone"
-            static var microphonePermissionDescription: String = "Square Reader for magstripe uses the microphone to communicate payment card data to your device. You should ask for this permission if you are using a magstripe reader."
+            static var microphonePermissionTitle: String = NSLocalizedString("Microphone", comment: "Microphone permission title")
+            static var microphonePermissionDescription: String = NSLocalizedString("Square Reader for magstripe uses the microphone to communicate payment card data to your device. You should ask for this permission if you are using a magstripe reader.", comment: "Microphone permission description")
         }
         
         enum AuthorizationButton {
-            static var authorizeTitle: String = "Sign In"
-            static var deauthorizeTitle: String = "Sign out"
+            static var authorizeTitle: String = NSLocalizedString("Sign In", comment: "Authorization button title")
+            static var deauthorizeTitle: String = NSLocalizedString("Sign out", comment: "Deauthorization button title")
         }
     }
 }
