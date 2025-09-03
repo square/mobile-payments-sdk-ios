b0725986f49c91382235ca46a95b041f3bec245e
Brandon Jenniges <brandonjenniges@squareup.com>
2025-09-03 13:31:22 -0500

    Fix sdk name in Config.swift

--- Example/Shared/Config.swift
+++ Example/Shared/Config.swift
@@ -1,8 +1,8 @@
 public enum Config {
-     static let squareApplicationID: String? = nil // Replace with your squareApplicationID
-    
+    static let squareApplicationID: String? = nil // Replace with your squareApplicationID
+
     // In a production application you should use your server to obtain an access token and locationID.
-    // For this sample app, we can just authorize Reader SDK using hardcoded values.
-     static let accessToken: String? = nil // Replace with your accessToken
-     static let locationID: String? = nil // Replace with your locationID
+    // For this sample app, we can just authorize Square Mobile Payments SDK using hardcoded values.
+    static let accessToken: String? = nil // Replace with your accessToken
+    static let locationID: String? = nil // Replace with your locationID
 }
