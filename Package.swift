ee4e1b696de47fa6926f2dac7eea69fac01417e7
Randy Beiter <rbeiter@squareup.com>
2026-04-10 14:46:28 -0400

    MPSDK 2.5.0

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SquareMobilePaymentsSDK",
-            url: "https://d3eygymyzkbhx3.cloudfront.net/mpsdk/2.4.0/SquareMobilePaymentsSDK_59854a3d09f8.zip",
-            checksum: "ad516f40a8230a2de3e8a46ec46b45ed5df7bfa6fcf729e4f18b75f538b2abd1"
+            url: "https://d3eygymyzkbhx3.cloudfront.net/mpsdk/2.5.0/SquareMobilePaymentsSDK_5b3a1df39f0a.zip",
+            checksum: "109737f24088707bb4b7ede4d0281a912255b5fd2717291b85a06d505117d70f"
         ),
         .binaryTarget(
             name: "MockReaderUI",
-            url: "https://d3eygymyzkbhx3.cloudfront.net/mpsdk/2.4.0/MockReaderUI_59854a3d09f8.zip",
-            checksum: "5ff1347a2f39c27367047f7328328db290f68a0f5723625c63d2167da44835b0"
+            url: "https://d3eygymyzkbhx3.cloudfront.net/mpsdk/2.5.0/MockReaderUI_5b3a1df39f0a.zip",
+            checksum: "2136835f8f0a6cdd95c90ff85c50bfdf11ae3dd7f58a9361dc2c7691f0488e7a"
         ),
     ]
 )
--- Scripts/sdk_constants.rb
+++ Scripts/sdk_constants.rb
@@ -1,6 +1,6 @@
 module SquareMobilePaymentsSDK
-  VERSION = '2.4.0'
-  COMMIT_SHA = '59854a3d09f8'
+  VERSION = '2.5.0'
+  COMMIT_SHA = '5b3a1df39f0a'
   CLOUDFRONT_DOMAIN = 'd3eygymyzkbhx3.cloudfront.net'
   LICENSE_TYPE = 'Square Developer License'
   LICENSE_TEXT = <<-LICENSE
